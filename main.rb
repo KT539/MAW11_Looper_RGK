529601db6cbeeed5c6e0e182bc645ee393596c47
ryan <pc23owx@eduvaud.ch>
2026-09-04 08:52:36 +0200

    fonctionality about the field is completed (left editable en deletable)

--- main.rb
+++ main.rb
@@ -4,10 +4,48 @@ require 'sinatra'
 require 'mysql2'
 require 'fileutils'
 
-client = Mysql2::Client.new(:host => ENV['DB_HOST'], :username => ENV['DB_USERNAME'], :password => ENV['DB_PASSWORD'], :database => ENV['DB_DATABASE'])
+PUBLIC_FOLDER = File.expand_path('Site_remastered', __dir__)
+FIELDS_TEMPLATE_PATH = File.join(PUBLIC_FOLDER, 'exercises', 'template', 'fields.html')
+FIELD_TYPES = %w[single_line single_line_list multi_line].freeze
 
-# Rend le dossier du site accessible depuis le même serveur que l'application Ruby.
-set :public_folder, File.expand_path('Site_remastered', __dir__)
+DB = Mysql2::Client.new(
+  host: ENV.fetch('DB_HOST'),
+  username: ENV.fetch('DB_USERNAME'),
+  password: ENV.fetch('DB_PASSWORD'),
+  database: ENV.fetch('DB_DATABASE')
+)
+
+set :public_folder, PUBLIC_FOLDER
+
+def fields_page_path(form_id)
+  File.join(PUBLIC_FOLDER, 'exercises', form_id.to_s, 'fields.html')
+end
+
+def generate_fields_page(form_id)
+  form = DB.prepare('SELECT name FROM forms WHERE id = ?').execute(form_id).first
+  return false unless form
+
+  labels = DB.prepare('SELECT label_name, type FROM labels WHERE form_id = ? ORDER BY id').execute(form_id)
+  label_rows = labels.map do |label|
+    <<~HTML
+      <tr>
+        <td>#{Rack::Utils.escape_html(label['label_name'])}</td>
+        <td>#{Rack::Utils.escape_html(label['type'])}</td>
+        <td></td>
+      </tr>
+    HTML
+  end.join
+
+  page_content = File.read(FIELDS_TEMPLATE_PATH)
+    .sub('[Forms title]', Rack::Utils.escape_html(form['name']))
+    .sub('<!-- LABEL_ROWS -->', label_rows)
+    .sub('<!-- FIELD_FORM_ACTION -->', "http://localhost:4567/exercises/#{form_id}/fields")
+
+  page_path = fields_page_path(form_id)
+  FileUtils.mkdir_p(File.dirname(page_path))
+  File.write(page_path, page_content)
+  true
+end
 
 get '/' do
   redirect '/exercises/new.html'
@@ -15,19 +53,30 @@ end
 
 post '/traitement' do
   title = params.dig('exercise', 'title').to_s.strip
-
   halt 422, 'Veuillez saisir un titre.' if title.empty?
 
-  # La requête préparée protège la base des caractères spéciaux et injections SQL.
-  client.prepare('INSERT INTO forms (name, status) VALUES (?, ?)').execute(title, 'Building')
-  form_id = client.last_id
+  DB.prepare('INSERT INTO forms (name, status) VALUES (?, ?)').execute(title, 'Building')
+  form_id = DB.last_id
+  generate_fields_page(form_id)
 
-  template_path = File.join(settings.public_folder, 'exercises', 'template', 'fields.html')
-  page_path = File.join(settings.public_folder, 'exercises', form_id.to_s, 'fields.html')
-  page_content = File.read(template_path).sub('[Forms title]', Rack::Utils.escape_html(title))
+  redirect "/exercises/#{form_id}/fields.html"
+end
 
-  FileUtils.mkdir_p(File.dirname(page_path))
-  File.write(page_path, page_content)
+post '/exercises/:form_id/fields' do
+  form_id = params[:form_id]
+  halt 404, 'Exercice introuvable.' unless form_id.match?(/\A[1-9]\d*\z/)
+
+  label_name = params.dig('field', 'label').to_s.strip
+  value_kind = params.dig('field', 'value_kind').to_s
+  halt 422, 'Veuillez saisir un libellé.' if label_name.empty?
+  halt 422, 'Type de valeur invalide.' unless FIELD_TYPES.include?(value_kind)
+
+  form_exists = DB.prepare('SELECT id FROM forms WHERE id = ?').execute(form_id).first
+  halt 404, 'Exercice introuvable.' unless form_exists
+
+  DB.prepare('INSERT INTO labels (label_name, type, form_id) VALUES (?, ?, ?)')
+    .execute(label_name, value_kind, form_id)
+  generate_fields_page(form_id)
 
   redirect "/exercises/#{form_id}/fields.html"
 end
